5a21b5c6ce54b3941fb4d7675540cec685593a6c
Jesse de Vries <jessedvrs@gmail.com>
2016-04-13 13:50:16 +0200

    feat(Spinner): add Spinner component and implement into a Button with loading state

--- app/imports/components/Button.jsx
+++ app/imports/components/Button.jsx
@@ -3,10 +3,13 @@
 import React from 'react';
 import c from 'classnames';
 
+import Spinner from './Spinner';
+
 export default class Button extends React.Component {
     render() {
         const className = c('pa-Button', {
             'pa-Button--text': this.props.text,
+            'pa-Button--loading': this.props.loading,
             'pa-Button--login': this.props.login,
             'pa-Button--login--facebook': this.props.loginFacebook,
             'pa-Button--login--linkedin': this.props.loginLinkedIn
@@ -15,6 +18,7 @@ export default class Button extends React.Component {
         return (
             <button type={this.props.submit ? "submit" : "button"} className={className} onClick={this.onClick.bind(this)}>
                 {this.props.children}
+                <Spinner button />
             </button>
         );
     }
@@ -31,10 +35,12 @@ export default class Button extends React.Component {
 Button.propTypes = {
     onClick: React.PropTypes.func,
     submit: React.PropTypes.bool,
-    text: React.PropTypes.bool
+    text: React.PropTypes.bool,
+    loading: React.PropTypes.bool
 };
 
 Button.defaultProps = {
     submit: false,
-    text: false
+    text: false,
+    loading: false
 };
---
+++ app/imports/components/Spinner.jsx
@@ -0,0 +1,49 @@
+'use strict';
+
+import React from 'react';
+import c from 'classnames';
+import ReactSpinner from 'react-spinjs';
+
+export default class Spinner extends React.Component {
+    render() {
+        const className = c('pa-Spinner', {
+            'pa-Spinner--button': this.props.button
+        });
+
+        const opts = {
+            lines: 13,  // The number of lines to draw
+            length: 6,  // The length of each line
+            width: 2,  // The line thickness
+            radius: 6,  // The radius of the inner circle
+            corners: 1,  // Corner roundness (0..1)
+            rotate: 0,  // The rotation offset
+            direction: 1,  // 1: clockwise, -1: counterclockwise
+            color: '#000',  // #rgb or #rrggbb
+            speed: 1.2,  // Rounds per second
+            trail: 60,  // Afterglow percentage
+            shadow: false,  // Whether to render a shadow
+            hwaccel: false,  // Whether to use hardware acceleration
+            className: className, // The CSS class to assign to the spinner
+            zIndex: 0,  // The z-index (defaults to 2000000000)
+            top: '50%',  // Top position relative to parent in px
+            left: 'auto'  // Left position relative to parent in px
+        };
+
+        if (this.props.button) {
+            opts.color = '#fff';
+            opts.width = 1;
+            opts.length = 4;
+            opts.radius = 4;
+        }
+
+        return <ReactSpinner config={opts} />;
+    }
+}
+
+Spinner.propTypes = {
+    button: React.PropTypes.bool
+};
+
+Spinner.defaultProps = {
+    button: false
+};
