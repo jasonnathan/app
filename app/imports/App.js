a605d5d93359af851ebe4ac2afbb9aa9865c66df
Jesse de Vries <jessedvrs@gmail.com>
2016-04-18 11:54:12 +0200

    docs(App.js): fix

--- app/imports/App.js
+++ app/imports/App.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// Use `App.get()`` in your code to get a reference to the touchstone app instance.
+// Use `App.get()` in your code to get a reference to the touchstone app instance.
 // You'll need it to programmatically transition to another page.
-// `App.set()`` is being called in RootView.jsx where the touchstone app is being created.
+// `App.set()` is being called in RootView.jsx where the touchstone app is being created.
 
 let app;
 
