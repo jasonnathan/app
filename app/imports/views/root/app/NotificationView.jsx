de3d789d6554b15303497f347107ff68a8251882
Jesse de Vries <jessedvrs@gmail.com>
2016-04-25 17:42:07 +0200

    fix(NotificationView): render same data as in tile

--- app/imports/views/root/app/NotificationView.jsx
+++ app/imports/views/root/app/NotificationView.jsx
@@ -11,20 +11,44 @@ import Paragraph from '/imports/components/Paragraph';
 import NotificationModel from '/imports/models/NotificationModel';
 
 const NotificationView = class NotificationView extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            nowDate: new Date()
+        };
+    }
+
+    componentDidMount() {
+        this.nowInterval = setInterval(this.updateNowDate.bind(this), 5000);
+    }
+
+    componentWillUnmount() {
+        if (this.nowInterval) {
+            clearInterval(this.nowInterval);
+            delete this.nowInterval;
+        }
+    }
+
+    updateNowDate() {
+        this.setState({
+            nowDate: new Date()
+        });
+    }
+
     render() {
         const {t, notification: n} = this.props;
+        const {nowDate} = this.state;
 
         return (
             <Container fill scrollable>
                 <UI.NavigationBar name="notification" className={"NavigationBar--detail"} />
                 <Container>
                     <Content>
-                        <Paragraph>{t(`notification-${n.type}`, {inviter: 'Jantje'})}</Paragraph>
-                        {Object.keys(n).map((key, index) => (
-                            <Content.Text key={index}>
-                                <Paragraph>{key}: {JSON.stringify(n[key])}</Paragraph>
-                            </Content.Text>
-                        ))}
+                        <Content.Text>
+                            <Paragraph><strong>{n.getText(t)}</strong></Paragraph>
+                            <Paragraph meta>{n.getMetaText(t, nowDate)}</Paragraph>
+                        </Content.Text>
                     </Content>
                 </Container>
             </Container>
