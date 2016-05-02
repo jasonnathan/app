99842c6974832ea2a8748f35ee198b2bba676b51
Jesse de Vries <jessedvrs@gmail.com>
2016-05-02 16:02:53 +0200

    feat(PartupUpdateModel): add static function to compose a user instance for a comment

--- app/imports/models/PartupUpdateModel.js
+++ app/imports/models/PartupUpdateModel.js
@@ -2,9 +2,18 @@
 
 import Model from '/imports/classes/Model';
 import linkCollection from '/imports/services/linkCollection';
+import UserModel from '/imports/models/UserModel';
 
 export default class PartupUpdateModel extends Model {
-    //
+    static getUserForComment(comment) {
+        return new UserModel({
+            _id: comment.creator._id,
+            profile: {
+                name: comment.creator.name,
+                avatar: comment.creator.image
+            }
+        });
+    }
 }
 
 linkCollection(PartupUpdateModel, 'updates');
