cb4bad901a90c7f530f4cd67b06b03d0b9ae1327
Jesse de Vries <jessedvrs@gmail.com>
2016-05-02 11:21:41 +0200

    feat(PartupUpdateModel): add model for updates collection

---
+++ app/imports/models/PartupUpdateModel.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import Model from '/imports/classes/Model';
+import linkCollection from '/imports/services/linkCollection';
+
+export default class PartupUpdateModel extends Model {
+    //
+}
+
+linkCollection(PartupUpdateModel, 'updates');
