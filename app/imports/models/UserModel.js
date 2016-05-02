4a1fe07be12c961760fc5327416aa7bc4908710b
Jesse de Vries <jessedvrs@gmail.com>
2016-05-02 16:02:25 +0200

    feat(UserModel): add ability to obtain the avatar image object directly from a user

--- app/imports/models/UserModel.js
+++ app/imports/models/UserModel.js
@@ -5,6 +5,7 @@ import { AccountsClient } from 'meteor/accounts-base';
 import Connection from '/imports/Connection';
 import Model from '/imports/classes/Model';
 import linkCollection from '/imports/services/linkCollection';
+import ImageModel from '/imports/models/ImageModel';
 
 const accountsClient = new AccountsClient({
     connection: Connection
@@ -28,6 +29,15 @@ export default class UserModel extends Model {
      */
     getFirstname() {
         return this.profile.firstname || this.profile.name.split(' ').shift();
+    }
+
+    /**
+     * Get the avatar image
+     *
+     * @return {ImageModel}
+     */
+    getAvatarImage() {
+        return this.profile.avatar && ImageModel.findOne(this.profile.avatar);
     }
 }
 
