8c4d88b285785b1d1e7c9d5e7644c85b098740e6
Anne Korteweg <anne_korteweg@hotmail.com>
2016-05-03 17:32:22 +0200

    add(partups page): add partups page

--- app/imports/views/root/app/tabs/TribesView.jsx
+++ app/imports/views/root/app/tabs/TribesView.jsx
@@ -4,14 +4,37 @@ import React from 'react';
 
 import Content from '/imports/components/Content';
 import Paragraph from '/imports/components/Paragraph';
+import Button from '/imports/components/Button';
+import PartupTile from '/imports/components/PartupTile';
+import List from '/imports/components/List';
+import ListItem from '/imports/components/ListItem';
 
 const TribesView = class TribesView extends React.Component {
     render() {
         return (
             <Content>
-                <Content.Text>
-                    <Paragraph>Tribes</Paragraph>
-                </Content.Text>
+                <Content.Block>
+                    <Button>Tribes</Button>
+                    <Button>Tribes</Button>
+                </Content.Block>
+                <Content.Block>
+                    <List>
+                        <ListItem>
+                            <PartupTile>
+                                <PartupTile.Image
+                                    src="http://media02.hongkiat.com/ww-flower-wallpapers/roundflower.jpg"
+                                    alt="Flower">
+                                </PartupTile.Image>
+                                <Paragraph>
+                                    Some text to make an example...
+                                </Paragraph>
+                                <PartupTile.Stats>
+                                    7
+                                </PartupTile.Stats>
+                            </PartupTile>
+                        </ListItem>
+                    </List>
+                </Content.Block>
             </Content>
         );
     }
