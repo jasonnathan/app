9fbc7129cb16a3e2bb13a76ff47dae44cb4170c0
Jesse de Vries <jessedvrs@gmail.com>
2016-05-05 22:35:40 +0200

    feat(Tile): add support for click handler

--- app/imports/components/Tile.jsx
+++ app/imports/components/Tile.jsx
@@ -6,8 +6,24 @@ import Paragraph from '/imports/components/Paragraph';
 
 const Tile = class Tile extends React.Component {
     render() {
+        if (this.props.onClick) {
+            return (
+                <a onClick={this.onClick.bind(this)} className="pa-Tile">
+                    {this.renderContent()}
+                </a>
+            );
+        }
+
         return (
             <div className="pa-Tile">
+                {this.renderContent()}
+            </div>
+        );
+    }
+
+    renderContent() {
+        return (
+            <div>
                 <figure className="pa-Tile__Image" style={{
                     backgroundImage: this.props.imageSrc && `url('${this.props.imageSrc}')`}}>
                 </figure>
@@ -20,12 +36,18 @@ const Tile = class Tile extends React.Component {
             </div>
         );
     }
+
+    onClick(event) {
+        event.preventDefault();
+        this.props.onClick();
+    }
 };
 
 Tile.propTypes = {
     imageSrc: React.PropTypes.string,
     label: React.PropTypes.string.isRequired,
-    updatesCount: React.PropTypes.number
+    updatesCount: React.PropTypes.number,
+    onClick: React.PropTypes.func
 };
 
 export default Tile;
