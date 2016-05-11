d7889ed3cfa6f361cb64afed5d82674a0a465880
Anne Korteweg <anne_korteweg@hotmail.com>
2016-05-11 14:10:47 +0200

    fix(chattile): fix unread state

--- app/imports/components/ChatTile.jsx
+++ app/imports/components/ChatTile.jsx
@@ -1,21 +1,26 @@
 'use strict';
 
 import React from 'react';
+import c from 'classnames';
 import Avatar from '/imports/components/Avatar';
 import Paragraph from '/imports/components/Paragraph';
 
 const ChatTile = class ChatTile extends React.Component {
     render() {
+        const className = c('pa-ChatTile', {
+            'pa-ChatTile--is-unread': true
+        });
+
         if (this.props.onClick) {
             return (
-                <a onClick={this.onClick.bind(this)} className="pa-ChatTile">
+                <a onClick={this.onClick.bind(this)} className={className}>
                     {this.renderContent()}
                 </a>
             );
         }
 
         return (
-            <div className="pa-ChatTile">
+            <div className={className}>
                 {this.renderContent()}
             </div>
         );
@@ -24,8 +29,18 @@ const ChatTile = class ChatTile extends React.Component {
     renderContent() {
         return (
             <div>
-                <Avatar src="http://media02.hongkiat.com/ww-flower-wallpapers/roundflower.jpg"></Avatar>
-
+                <div className="pa-ChatTile__image">
+                    <Avatar src="http://media02.hongkiat.com/ww-flower-wallpapers/roundflower.jpg"></Avatar>
+                </div>
+                <div className="pa-ChatTile__label">
+                    <Paragraph className="pa-ChatTile__label__title">Anthony Wells</Paragraph>
+                    <Paragraph>Lorem ipsum dolor sit amet dolor sit</Paragraph>
+                </div>
+                <span className="pa-ChatTile__time">
+                    <Paragraph>Yesterday</Paragraph>
+                </span>
+                <span className="pa-ChatTile__alert">
+                </span>
             </div>
         );
     }
@@ -37,10 +52,7 @@ const ChatTile = class ChatTile extends React.Component {
 };
 
 ChatTile.propTypes = {
-    imageSrc: React.PropTypes.string,
-    label: React.PropTypes.string.isRequired,
-    updatesCount: React.PropTypes.number,
-    onClick: React.PropTypes.func
+
 };
 
 export default ChatTile;
