f03eb9bc5681b585d696c530a6ca0b2ec3e2be5f
Jesse de Vries <jessedvrs@gmail.com>
2016-06-14 17:32:59 +0200

    fix(payload-handler): fix payload interpretation

--- app/imports/views/root/PushNotificationContainer.jsx
+++ app/imports/views/root/PushNotificationContainer.jsx
@@ -10,16 +10,19 @@ import Subs from '/imports/Subs';
 import openWeb from '/imports/services/openWeb';
 import { NotificationModel } from '/imports/models';
 import Connection from '/imports/Connection';
+import { get } from 'mout/object';
 
 export default meteorDataContainer(PushNotificationView, (props) => {
     const {payload} = props;
     Debug.tracker('PushNotificationContainer', payload);
 
     try {
+        const payloadString = get(payload, 'additionalData.payload');
+        const parsedPayload = typeof payloadString === 'object' ? payloadString : JSON.parse(payloadString);
 
         // Chat message
-        if (payload.additionalData.chat) {
-            const chat = payload.additionalData.chat;
+        if (parsedPayload.chat) {
+            const chat = parsedPayload.chat;
 
             transitionTo('root:app:chat', {
                 transition: 'instant',
@@ -31,14 +34,12 @@ export default meteorDataContainer(PushNotificationView, (props) => {
         }
 
         // Notification
-        else if (payload.additionalData.notification) {
-            const notification = payload.additionalData.notification;
-            console.log('notification', notification);
+        else if (parsedPayload.notification) {
+            const notification = parsedPayload.notification;
 
             Subs.subscribe('notifications.for_upper.by_id', notification._id, {
                 onReady: () => {
                     const _notification = NotificationModel.findOne(notification._id);
-                    console.log('_notification', _notification);
 
                     if (!_notification) {
                         throw 'Notification not found';
@@ -47,7 +48,6 @@ export default meteorDataContainer(PushNotificationView, (props) => {
                     Connection.call('notifications.clicked', _notification._id);
 
                     if (_notification.hasUpdate()) {
-                        console.log('transitionTo(`root:app:notification`)');
                         transitionTo('root:app:notification', {
                             transition: 'instant',
                             viewProps: {
@@ -55,7 +55,6 @@ export default meteorDataContainer(PushNotificationView, (props) => {
                             }
                         });
                     } else {
-                        console.log('openWeb');
                         openWeb(_notification.getWebsiteUrl());
                     }
                 }
@@ -70,7 +69,7 @@ export default meteorDataContainer(PushNotificationView, (props) => {
     } catch(err) {
         console.error(err);
 
-        transitionTo('app:tabs:notifications', {
+        transitionTo('root:app:tabs:notifications', {
             transition: 'fade',
             viewProps: {}
         });
