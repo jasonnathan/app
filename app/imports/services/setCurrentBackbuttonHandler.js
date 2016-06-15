734138f4ebf13136265c51cb5260dc10ffe34f48
Jesse de Vries <jessedvrs@gmail.com>
2016-06-15 15:27:41 +0200

    feat(swipe-back): add swipedetect function to detect a swipe-right to go back

--- app/imports/services/setCurrentBackbuttonHandler.js
+++ app/imports/services/setCurrentBackbuttonHandler.js
@@ -1,3 +1,5 @@
+import swipedetect from '/imports/services/swipedetect';
+
 let currentBackbuttonHandler;
 
 const handler = () => {
@@ -9,9 +11,11 @@ const handler = () => {
 export default function setCurrentBackbuttonHandler(_handler) {
     currentBackbuttonHandler = _handler;
 
-    if (_handler) {
+    if (currentBackbuttonHandler) {
         document.addEventListener('backbutton', handler, false);
+        swipedetect.on('right', handler);
     } else {
         document.removeEventListener('backbutton', handler, false);
+        swipedetect.off('right', handler);
     }
 };
