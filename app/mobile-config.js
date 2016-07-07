ec96e2739d0f46804c4005c6a522b429ac8a5568
Jesse de Vries <jessedvrs@gmail.com>
2016-07-07 16:23:24 +0200

    Update versions in mobile-config and package.json

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -1,7 +1,7 @@
 App.info({
     id: 'com.partup.app',
     name: 'Part-up',
-    version: '1.2.4',
+    version: '1.3.0',
     description: 'Enhance your Part-up experience with the app.',
     author: 'Part-up',
     email: 'info@part-up.com',
