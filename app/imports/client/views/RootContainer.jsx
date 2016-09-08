3af6f21eeaa59f595df6a15978dc72ede1b99134
Jesse de Vries <jessedvrs@gmail.com>
2016-09-08 14:20:51 +0200

    feat(3d-touch): implement Quick Actions for new iPhone models

--- app/imports/client/views/RootContainer.jsx
+++ app/imports/client/views/RootContainer.jsx
@@ -1,10 +1,63 @@
 'use strict';
 
 import { Meteor } from 'meteor/meteor';
+import { contains } from 'mout/array';
 import meteorDataContainer from '/imports/client/services/meteorDataContainer';
 import RootViewManager from './RootViewManager';
 import Debug from '/imports/client/Debug';
+import transitionTo from '/imports/client/services/transitionTo';
 import '/imports/client/Connection';
+import TabsViewManager from '/imports/client/views/root/app/TabsViewManager.jsx';
+
+Meteor.startup(() => {
+    if ('ThreeDeeTouch' in window) {
+        ThreeDeeTouch.isAvailable((avail) => {
+            if (avail) {
+                ThreeDeeTouch.enableLinkPreview();
+                ThreeDeeTouch.configureQuickActions([
+                    {
+                        type: 'notifications',
+                        title: 'Notifications',
+                        // subtitle: '',
+                        iconType: 'Time'
+                    },
+                    {
+                        type: 'chats',
+                        title: 'Chats',
+                        // subtitle: '',
+                        iconType: 'Message'
+                    },
+                    {
+                        type: 'partups',
+                        title: 'Part-ups'
+                    },
+                    {
+                        type: 'tribes',
+                        title: 'Tribes'
+                    }
+                ]);
+
+                ThreeDeeTouch.onHomeIconPressed = (payload) => {
+                    const TABS = [
+                        'notifications',
+                        'chats',
+                        'partups',
+                        'tribes'
+                    ];
+
+                    // Open tab
+                    if (contains(TABS, payload.type)) {
+                        TabsViewManager.lastSelectedTab = payload.type;
+                        transitionTo(`root:app:tabs:${payload.type}`, {
+                            transition: 'fade'
+                        });
+                    }
+                };
+            }
+        });
+
+    }
+});
 
 export default meteorDataContainer(RootViewManager, (props) => {
     const {} = props;
@@ -14,3 +67,4 @@ export default meteorDataContainer(RootViewManager, (props) => {
         userId: Meteor.userId()
     };
 });
+
