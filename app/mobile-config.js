02da7f77dabef21a2cba02a5627f33fbced11c5f
Jesse de Vries <jessedvrs@gmail.com>
2016-09-09 17:38:53 +0200

    Bump version to 1.5.0

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -1,7 +1,7 @@
 App.info({
     id: 'com.partup.app',
     name: 'Part-up',
-    version: '1.4.1',
+    version: '1.5.0',
     description: 'Enhance your Part-up experience with the app.',
     author: 'Part-up',
     email: 'info@part-up.com',
